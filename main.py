91e9bca4cda7dafe5fb3c1e6fcd258711a6d0d38
Free Python Code <71100436+amr2018@users.noreply.github.com>
2023-04-18 15:47:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,24 @@
 # the main video from me or you if you have this script 🙃
 
 import moviepy.editor as mp
+from os import listdir
 
-# load videos
-main_video_path = 'youtube video editor script add intro and end to your youtube videos.mp4'
-main_video = mp.VideoFileClip(main_video_path) # your video here 🙂
-video_width, video_height = main_video.w, main_video.h
+videos_folder = listdir('videos')
 
-# add start and end to main video
+for i, video_name in enumerate(videos_folder):
+	# load videos
+	main_video_path = 'videos/'  + video_name
+	main_video = mp.VideoFileClip(main_video_path) # your video here 🙂
+	video_width, video_height = main_video.w, main_video.h
 
-start_video = mp.VideoFileClip('start.mp4').resize((video_width, video_height))
-end_video = mp.VideoFileClip('end.mp4').resize((video_width, video_height))
+	# add start and end to main video
+	print(f'start editing video : {video_name} -- {i + 1} : {len(videos_folder)} video')
 
-full_video = mp.concatenate_videoclips([start_video, main_video, end_video], method="compose")
-full_video.write_videofile('out ' + main_video_path, verbose=False, logger=None)
+	start_video = mp.VideoFileClip('start.mp4').resize((video_width, video_height))
+	end_video = mp.VideoFileClip('end.mp4').resize((video_width, video_height))
+
+	full_video = mp.concatenate_videoclips([start_video, main_video, end_video], method="compose")
+	full_video.write_videofile('out/' + video_name, verbose=False, logger=None)
+
+
 print('The videos have been merged successfully ;)')
